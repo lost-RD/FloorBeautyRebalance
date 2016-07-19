d461358f3756b97f2ff3e9296439bbece0a4cf40
lost-RD <lost_RD@live.com>
2016-07-20 06:28:29 +0930

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FloorBeautyRebalance
-RimWorld mod
+RimWorld mod for Spartyon7
+WoodPlankFloor now gives 2 beauty instead of none. Smoothed floors now give 1 beauty (down from 3) and take 400 work to make (down from 1200).
+
+##Changelog:
+
+###1.0:
+* threw it together in 2 hours
+* things seem to work
---
+++ Source/FloorBeautyRebalance/ModifyDefs.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using Verse;
+
+namespace FloorBeautyRebalance
+{
+    [StaticConstructorOnStartup]
+    public class ModifyDefs
+    {
+
+        public static void ModifyWoodPlanks()
+        {
+            float beauty = 2f;
+            DefDatabase<TerrainDef>.GetNamed("WoodPlankFloor", true).SetStatBaseValue(StatDefOf.Beauty, beauty);
+        }
+
+        public static void ModifySmoothedStone()
+        {
+            float beauty = 1f;
+            DefDatabase<TerrainDef>.GetNamed("Sandstone_Smooth", true).SetStatBaseValue(StatDefOf.Beauty, beauty);
+            DefDatabase<TerrainDef>.GetNamed("Limestone_Smooth", true).SetStatBaseValue(StatDefOf.Beauty, beauty);
+            DefDatabase<TerrainDef>.GetNamed("Granite_Smooth", true).SetStatBaseValue(StatDefOf.Beauty, beauty);
+            DefDatabase<TerrainDef>.GetNamed("Marble_Smooth", true).SetStatBaseValue(StatDefOf.Beauty, beauty);
+            DefDatabase<TerrainDef>.GetNamed("Slate_Smooth", true).SetStatBaseValue(StatDefOf.Beauty, beauty);
+        }
+
+        static ModifyDefs()
+        {
+            ModifyWoodPlanks();
+            ModifySmoothedStone();
+        }
+    }
+}
